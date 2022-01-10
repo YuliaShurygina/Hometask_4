c1068c5e4598c1d17fba68c5b8d9391aa8aae78c
Yulia Shurygina <zavalishina-yu@mail.ru>
2022-01-10 13:15:27 +0300

    Random array filled with three digits numbers, find quantity of even and odd numbers

---
+++ task_36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+int[] EnterArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+        array[i] = new Random().Next(100,1000);
+    return array;
+}
+
+void PrintArray(int[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+        Console.Write($"  {Array[i]}  ");
+}
+
+int EvenNumbers(int[] SomeArray)
+{
+    int count = 0;
+    for (int i = 0; i < SomeArray.Length; i++)
+    {
+        if (SomeArray[i] % 2 == 0)
+            count = count + 1;
+    }
+    return count;
+}
+
+int[] SpecifiedArray = EnterArray(15);
+PrintArray(SpecifiedArray);
+Console.WriteLine();
+int EvenQuantity = EvenNumbers(SpecifiedArray);
+Console.WriteLine($"Количество четных чисел в массиве равно {EvenQuantity} ");
+Console.WriteLine($"Количество нечетных чисел в массиве равно {SpecifiedArray.Length - EvenQuantity} ");
