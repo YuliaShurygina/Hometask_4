84eee83b70ddbe76a2d99851708748d555677a20
Yulia Shurygina <zavalishina-yu@mail.ru>
2022-01-10 18:40:33 +0300

    Difference between max and min elements

---
+++ task_40/Program.cs
@@ -0,0 +1,52 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+double [] EnterArray(int length)
+{
+    double[] array = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = (new Random()).Next(0, 11400) / 1000.0;
+    }
+    return array;
+}
+
+void PrintArray(double[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($" {Array[i]} ");
+    }
+}
+
+double FindMax(double[] NewArr)
+{
+    double Max = NewArr[0];
+    for (int i = 1; i < NewArr.Length; i++)
+    {
+        if (NewArr[i] > Max)
+            Max = NewArr[i];
+    }
+    return Max;
+}
+
+double FindMin(double[] Arr)
+{
+    double Min = Arr[0];
+    for (int i = 1; i < Arr.Length; i++)
+    {
+        if (Arr[i] < Min)
+            Min = Arr[i];
+    }
+    return Min;
+}
+
+Console.Write($"Введите длину массива: ");
+int N = int.Parse(Console.ReadLine() ??"0");
+double [] NewArray = EnterArray(N);
+PrintArray(NewArray);
+double max = FindMax(NewArray);
+Console.WriteLine();
+Console.WriteLine($"Максимальный элемент равен {max}");
+double min = FindMin(NewArray);
+Console.WriteLine();
+Console.WriteLine($"Минимальный элемент равен {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом составляет: {max - min}");
